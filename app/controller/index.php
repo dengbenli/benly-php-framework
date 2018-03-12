fd5810d41aaa71d93d7bd12a2821d8cf4b782957
功夫集团-邓本利 <benli.deng@zkungfu.com>
2018-03-12 17:23:02 +0800

    update databases

--- app/controller/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class Index extends Controller
-{
-	public function home () {
-		echo 'index';
-	}
-}
--- core/init.php
+++ core/init.php
@@ -3,8 +3,6 @@
 require_once('../vendor/autoload.php');
 // init app
 require_once('../core/App.php');
-// init db
-require_once('../core/DB.php');
 // init controller
 require_once('../core/Controller.php');
 
