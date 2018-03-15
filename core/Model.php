8f9cf24314c5cf022723593da5c38b9da7e26336
功夫集团-邓本利 <benli.deng@zkungfu.com>
2018-03-15 11:51:13 +0800

    update Model.php

--- core/Model.php
+++ core/Model.php
@@ -5,4 +5,20 @@ class Model extends Mysql
 	public function __construct () {
 		
 	}
+
+	public function query () {
+
+	}
+
+	public function insert () {
+
+	}
+
+	public function delete () {
+
+	}
+
+	public function update () {
+		
+	}
 }
